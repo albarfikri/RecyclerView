c52a256e6f7bedff25a52dd05951d22f9bda32ac
albarfikri4 <fikrialbar4@gmail.com>
2021-03-06 22:06:38 +0700

    finished working on three parts of recycler view

--- app/src/main/java/com/example/myrecyclerview/GridHeroAdapter.kt
+++ app/src/main/java/com/example/myrecyclerview/GridHeroAdapter.kt
@@ -4,42 +4,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.TextView
+import androidx.recyclerview.widget.AdapterListUpdateCallback
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 
-class ListHeroAdapter(private val listHero: ArrayList<Hero>) :
-    RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+class GridHeroAdapter(val listHero: ArrayList<Hero>) :
+    RecyclerView.Adapter<GridHeroAdapter.GridViewHolder>() {
     private lateinit var onItemClickCallback: OnItemClickCallback
 
     fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
         this.onItemClickCallback = onItemClickCallback
     }
 
-    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
-        var tvDetail: TextView = itemView.findViewById(R.id.tv_item_detail)
+    inner class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
-
     }
 
-    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ListViewHolder {
-        val view: View =
-            LayoutInflater.from(viewGroup.context).inflate(R.layout.item_row_hero, viewGroup, false)
-        return ListViewHolder(view)
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): GridViewHolder {
+        val view: View = LayoutInflater.from(p0.context).inflate(R.layout.item_grid_hero, p0, false)
+        return GridViewHolder(view)
     }
 
-    override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        val hero = listHero[position]
-
+    override fun onBindViewHolder(holder: GridViewHolder, position: Int) {
         Glide.with(holder.itemView.context)
-            .load(hero.photo)
-            .apply(RequestOptions().override(55, 55))
+            .load(listHero[position].photo)
+            .apply(RequestOptions().override(350, 350))
             .into(holder.imgPhoto)
 
-        holder.tvName.text = hero.name
-        holder.tvDetail.text = hero.detail
         holder.itemView.setOnClickListener { onItemClickCallback.onItemClicked(listHero[holder.adapterPosition]) }
     }
 
--- app/src/main/java/com/example/myrecyclerview/MainActivity.kt
+++ app/src/main/java/com/example/myrecyclerview/MainActivity.kt
@@ -4,26 +4,63 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rvHeroes: RecyclerView
     private var list: ArrayList<Hero> = arrayListOf()
+    var title: String = "Mode List"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        setActionBarTitle(title)
         rvHeroes = findViewById(R.id.rv_heroes)
         rvHeroes.setHasFixedSize(true)
 
         list.addAll(HeroesData.listData)
         showRecyclerList()
     }
-    private fun showRecyclerList(){
+
+    //set title
+    private fun setActionBarTitle(title: String) {
+        supportActionBar?.title = title
+    }
+
+    //recyler mode: list
+    private fun showRecyclerList() {
         rvHeroes.layoutManager = LinearLayoutManager(this)
         val listHeroAdapter = ListHeroAdapter(list)
         rvHeroes.adapter = listHeroAdapter
+
+        listHeroAdapter.setOnItemClickCallback(object : ListHeroAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Hero) {
+                showSelectedHero(data)
+            }
+        })
+    }
+
+    //recyler mode:card
+    private fun showRecyclerGrid() {
+        rvHeroes.layoutManager = GridLayoutManager(this, 2)
+        val gridHeroAdapter = GridHeroAdapter(list)
+        rvHeroes.adapter = gridHeroAdapter
+
+        gridHeroAdapter.setOnItemClickCallback(object : GridHeroAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Hero) {
+                showSelectedHero(data)
+            }
+        })
+    }
+
+    //recyler mode:cardView
+    private fun showRecyclerCardView() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val cardViewHeroAdapter = CardViewHeroAdapter(list)
+        rvHeroes.adapter = cardViewHeroAdapter
     }
 
     // show the menu
@@ -32,24 +69,33 @@ class MainActivity : AppCompatActivity() {
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem):Boolean{
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
         setMode(item.itemId)
         return super.onOptionsItemSelected(item)
     }
 
-    private fun setMode(selectedMode: Int){
-        when(selectedMode){
-            R.id.action_list -> {
+    //Hero being selected method
+    private fun showSelectedHero(hero: Hero) {
+        Toast.makeText(this, "Kamu memilih " + hero.name, Toast.LENGTH_SHORT).show()
+    }
 
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                title = "Mode List"
+                showRecyclerList()
             }
 
             R.id.action_grid -> {
-
+                title = "Mode Grid"
+                showRecyclerGrid()
             }
 
             R.id.action_cardview -> {
-
+                title = "Mode CardView"
+                showRecyclerCardView()
             }
         }
+        setActionBarTitle(title)
     }
 }
